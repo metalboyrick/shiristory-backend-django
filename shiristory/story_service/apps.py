82b6dde1335e32ff9b5040dc9ec12b5426dcc6da
hengsoo <hengsoo.sjh@gmail.com>
2021-05-14 14:15:42 +0800

    build: djongo boilerplates

---
+++ shiristory/story_service/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StoryServiceConfig(AppConfig):
+    name = 'story_service'
---
+++ shiristory/timeline_service/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TimelineServiceConfig(AppConfig):
+    name = 'timeline_service'
