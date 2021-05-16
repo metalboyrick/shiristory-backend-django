ad666917366386f16c0e5cfeef76cf01fcd1d57b
DESKTOP-S551RPR\Richard <rsulisthio@gmail.com>
2021-05-16 18:14:56 +0700

    feat: added placeholder function for the get group info api

--- shiristory/story_service/urls.py
+++ shiristory/story_service/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:group_id>/info/', views.get_group_info, name='Get Group Info')
 ]
--- shiristory/story_service/views.py
+++ shiristory/story_service/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the story index.")
+
+
+def get_group_info(request, group_id):
+    return JsonResponse({'current_group_id': group_id})
