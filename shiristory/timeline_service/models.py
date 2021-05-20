9d3118bbb7afcd218d15a1d4817312ef29fe1ca2
hengsoo <hengsoo.sjh@gmail.com>
2021-05-20 16:10:16 +0800

    feature: add timeline model

--- shiristory/timeline_service/models.py
+++ shiristory/timeline_service/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    # TODO: Integrate with user
+    author = models.CharField(max_length=255)
+    comment = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    objects = models.DjongoManager()
+
+    class Meta:
+        abstract = True
+
+
+class Post(models.Model):
+    _id = models.ObjectIdField()
+    # TODO: Integrate with user
+    author = models.CharField(max_length=255)
+    content = models.CharField(max_length=500)
+    inv_link = models.URLField()
+    media = models.URLField()
+    media_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    # likes = models.ArrayReferenceField()
+    comments = models.EmbeddedField(model_container=Comment)
+
+    objects = models.DjongoManager()
