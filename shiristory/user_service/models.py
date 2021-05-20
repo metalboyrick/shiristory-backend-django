12d78bb46be77940505590106a5d13c0d3c6a3ee
ngjuping <ngjuping2018@163.com>
2021-05-20 23:13:07 +0800

    feat: add django auth extended user model

--- shiristory/user_service/models.py
+++ shiristory/user_service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pic_url = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    bio = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.student.username
