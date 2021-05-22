d776c40f414a51125359f82708a117b8dcd7d51e
ngjuping <ngjuping2018@163.com>
2021-05-22 15:48:45 +0800

    fix: replace session auth with JWT auth for login and signup

--- shiristory/user_service/urls.py
+++ shiristory/user_service/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login_view, name='login'),
-    path('signup', views.signup_view, name='sign up'),
+    path('signup', views.signup_view, name='sign-up'),
     path('logout', views.logout_view, name='logout'),
-    path('checkLoginStatus', views.testLoginView, name='check login status'),
-    path('jwt/token', TokenObtainPairView.as_view()),
-    path('jwt/token/refresh', TokenRefreshView.as_view()),
+    path('whoami', views.whoami_view, name='check-login-status'),
+    path('jwt/refresh', TokenRefreshView.as_view(), name='refresh-token'),
     # path('<str:group_id>/info/', views.get_group_info, name='Get Group Info'),
 ]
--- shiristory/user_service/views.py
+++ shiristory/user_service/views.py
@@ -1,59 +1,48 @@
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
-from .models import Profile
 from django.views.decorators.csrf import csrf_exempt
+from .models import Profile
 import json
 
 # JWT imports
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
-from rest_framework_simplejwt.tokens import AccessToken
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
 
+@api_view(['POST'])
+@permission_classes(())
+@authentication_classes(())
 @csrf_exempt
 def login_view(request):
-    if request.method != 'POST':
-        return JsonResponse({"message": "400 Unsupported HTTP method for this route"}, status=400)
+    # We are adding extra message to the response of simplejwt token generating view
+    res = (TokenObtainPairView.as_view()(request._request))
+    status_code = res.status_code
+    if status_code == 200:
+        res.data["message"] = "200 Login OK"
+    else:
+        res.data["message"] = str(status_code) + " Login Failed"
 
-    json_data = json.loads(request.body)
-    try:
-        username = json_data['username']
-        password = json_data['password']
-    except KeyError:
-        return JsonResponse({"message": "400 Missing login fields"}, status=400)
+    return res
 
-    user = authenticate(request, username=username, password=password)
-
-    # TODO Log user in by sending back a JWT token, we are not using session!
-    if user is not None:
-        login(request, user)
-        return JsonResponse({"message": "200 login OK"})
-
-    return JsonResponse({"message": "401 mismatched login credentials " + username + " " + password},status=401)
-
+@api_view(['POST'])
+@permission_classes(())
+@authentication_classes(())
 @csrf_exempt
 def signup_view(request):
-    if request.method != 'POST':
-        return JsonResponse({"message": "400 Unsupported HTTP method for this route"}, status=400)
-
     json_data = json.loads(request.body)
-
     try:
         username = json_data['username']
         password = json_data['password']
-        email = json_data['email']
+        email = json_data['username']
     except KeyError:
         return JsonResponse({"message": "400 Missing sign up fields"}, status=400)
 
     if User.objects.filter(username=username).exists():
         return JsonResponse({"message": "409 Signup user already exists"},status=409)
-
     else:
         new_user = User.objects.create_user(username=username,
                                         email=email,
@@ -61,25 +50,22 @@ def signup_view(request):
         new_profile = Profile(user=new_user)
         new_profile.save()
 
-        # TODO Log user in by sending back a JWT token
-        return JsonResponse({"message": "200 Signup OK"})
+        return login_view(request._request)
 
 @api_view(['GET'])
-@authentication_classes((JWTAuthentication,))
-def testLoginView(request):
+def whoami_view(request):
     if request.user.is_authenticated:
         return JsonResponse({"message": "Logged In as " + request.user.username})
     return JsonResponse({"message": "NOT Logged In"})
 
 @csrf_exempt
 @api_view(['POST'])
-@authentication_classes((JWTAuthentication,))
 def logout_view(request):
     if not request.user.is_authenticated:
         return JsonResponse({"message": "NOT Logged In"})
 
     # blacklist refresh_token
-    refresh_token = OutstandingToken.objects.get(user=request.user)
+    refresh_token = OutstandingToken.objects.filter(user=request.user).latest('id')
     blacklisted_token = BlacklistedToken(token=refresh_token)
     blacklisted_token.save()
 
