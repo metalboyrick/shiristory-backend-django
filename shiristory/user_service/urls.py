c74680af78ef717c4ef4c78c2e0ffaee59668839
ngjuping <ngjuping2018@163.com>
2021-05-25 21:41:59 +0800

    fix: change login url

--- shiristory/user_service/urls.py
+++ shiristory/user_service/urls.py
@@ -4,7 +4,7 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
-    path('jwt/token', TokenObtainPairView.as_view(), name='create_tokens'),
+    path('login', TokenObtainPairView.as_view(), name='create_tokens'),
     path('jwt/refresh', TokenRefreshView.as_view(), name='refresh_token'),
     path('signup', views.signup_view, name='sign_up'),
     path('reset', views.reset_password_view, name='reset_password'),
