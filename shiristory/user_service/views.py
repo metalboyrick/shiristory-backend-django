ad863c50bd85d5d9270c42cef4861116a683fc65
ngjuping <ngjuping2018@163.com>
2021-05-31 15:53:52 +0800

    fix: cannot add self as friend

--- shiristory/user_service/views.py
+++ shiristory/user_service/views.py
@@ -6,7 +6,11 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # JWT imports
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
 
 from shiristory import settings
 from shiristory.base.toolkits import save_uploaded_medias
@@ -16,44 +20,48 @@ from shiristory.user_service.models import User
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 @permission_classes(())
 @authentication_classes(())
 def signup_view(request):
     json_data = json.loads(request.body)
     try:
-        username = json_data['username']
-        password = json_data['password']
-        email = json_data['email']
+        username = json_data["username"]
+        password = json_data["password"]
+        email = json_data["email"]
 
     except KeyError:
         return JsonResponse({"message": "400 Missing sign up fields"}, status=400)
 
     # Fill fields for a shiristory profile
-    nickname = json_data['username']
+    nickname = json_data["username"]
     bio = "Default Bio"
-    profile_pic_url = f'{settings.APP_URL}:{settings.APP_PORT}{settings.MEDIA_URL}sample.jpg'
+    profile_pic_url = (
+        f"{settings.APP_URL}:{settings.APP_PORT}{settings.MEDIA_URL}sample.jpg"
+    )
 
     if User.objects.filter(username=username).exists():
         return JsonResponse({"message": "409 Signup user already exists"}, status=409)
     else:
-        new_user = User(username=username,
-                        email=email,
-                        nickname=nickname,
-                        bio=bio,
-                        profile_pic_url=profile_pic_url
-                        )
+        new_user = User(
+            username=username,
+            email=email,
+            nickname=nickname,
+            bio=bio,
+            profile_pic_url=profile_pic_url,
+        )
         new_user.set_password(password)
         new_user.save()
 
         return JsonResponse({"message": "Sign up OK"})
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def reset_password_view(request):
     json_data = json.loads(request.body)
     try:
-        new_password = json_data['new_password']
+        new_password = json_data["new_password"]
     except KeyError:
         return JsonResponse({"message": "400 Missing new password field"}, status=400)
 
@@ -75,24 +83,28 @@ def reset_password_view(request):
     return JsonResponse({"message": "200 Reset password OK"})
 
 
-@api_view(['GET', 'PUT'])
+@api_view(["GET", "PUT"])
 def profile_view(request):
-    logged_in_user = request.user
+    # TODO Change logged_in_user to request.user
+    logged_in_user = User.objects.all()[1]
 
-    if request.method == 'GET':
-        return JsonResponse({
-            "message": "200 get profile details OK",
-            "user": logged_in_user.to_dict(exclude=['password'])
-        })
+    if request.method == "GET":
+        return JsonResponse(
+            {
+                "message": "200 get profile details OK",
+                "user": logged_in_user.to_dict(exclude=["password"]),
+            }
+        )
     # PUT request
     else:
-
-        logged_in_user.nickname = request.data.get("new_nickname", logged_in_user.nickname)
+        logged_in_user.nickname = request.data.get(
+            "new_nickname", logged_in_user.nickname
+        )
         logged_in_user.bio = request.data.get("new_bio", logged_in_user.bio)
 
         if len(request.FILES) != 0:
             try:
-                media = save_uploaded_medias(request, 'user')
+                media = save_uploaded_medias(request, "user")
                 url = media[0]
                 logged_in_user.profile_pic_url = url
 
@@ -104,10 +116,15 @@ def profile_view(request):
         return JsonResponse({"message": "Update profile info OK"})
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def add_friend(request, friend_username):
     try:
-        logged_in_user = request.user
+        # TODO Change logged_in_user to request.user
+        logged_in_user = User.objects.all()[1]
+
+        if friend_username == logged_in_user.username:
+            return JsonResponse({"message": "Cannot add self as friend"})
+
         friends = logged_in_user.friends
         friends_list = list(friends.get_queryset())
         new_friend = User.objects.get(username=friend_username)
@@ -125,7 +142,7 @@ def add_friend(request, friend_username):
         return JsonResponse({"message": "Invalid Id"})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def search_friend(request, query):
     try:
         logged_in_user = request.user
@@ -135,11 +152,8 @@ def search_friend(request, query):
         matched = []
         for friend in friends_list:
             if query in friend.nickname:
-                matched.append(friend.to_dict(exclude=['password']))
-        return JsonResponse({
-            "message": "Search friend OK",
-            "candidates": matched
-        })
+                matched.append(friend.to_dict(exclude=["password"]))
+        return JsonResponse({"message": "Search friend OK", "candidates": matched})
     except User.DoesNotExist:
         return JsonResponse({"message": "User does not exist"})
 
@@ -147,7 +161,7 @@ def search_friend(request, query):
         return JsonResponse({"message": "Invalid Id"})
 
 
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def delete_friend(request, friend_id):
     try:
         logged_in_user = request.user
